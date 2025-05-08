c388a45803680176a202e4e7d603e1b42a8b03c5
Wasim Hossain <wasim.hossain003@gmail.com>
2025-05-08 08:40:32 +0600

    Problem-3 done

--- assignment.ts
+++ assignment.ts
@@ -3,8 +3,21 @@ function formatString(input: string, toUpper?: boolean = true):string{
     return toUpper? input.toUpperCase(): input.toLowerCase()
 }
 
-// problem-02
+// Problem-02
 
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
     return items.filter(item=> item.rating>=4)
 }
+
+// Problem-3
+
+function concatenateArrays<T>(...arrays: T[][]): T[]{
+    let result:T[] = []
+    arrays.forEach(array=>{
+        array.forEach(item=>{
+            result.push(item)
+        })
+    })
+    return result;
+}
+
