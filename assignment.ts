a8f4c95b16a507591912712a50fb683950ebacfd
Wasim Hossain <wasim.hossain003@gmail.com>
2025-05-08 16:13:52 +0600

    all problem almost solved

--- assignment.ts
+++ assignment.ts
@@ -18,35 +18,35 @@ function concatenateArrays<T>(...arrays: T[][]): T[]{
             result.push(item)
         })
     })
-    return result;
+    return result
 }
 
 // Problem-04
 
 class Vehicle{
-    private make: string;
-    private year: number;
+    private make: string
+    private year: number
   
     constructor(make: string, year: number){
-      this.make = make;
-      this.year = year;
+      this.make = make
+      this.year = year
     }
   
     public getInfo(): string{
-      return `Make: ${this.make}, Year: ${this.year}`;
+      return `Make: ${this.make}, Year: ${this.year}`
     }
   }
   
   class Car extends Vehicle {
-    private model: string;
+    private model: string
   
     constructor(make: string, year: number, model: string){
-      super(make, year);
-      this.model = model;
+      super(make, year)
+      this.model = model
     }
   
     public getModel(): string{
-      return `Model: ${this.model}`;
+      return `Model: ${this.model}`
     }
   }
 
@@ -55,9 +55,67 @@ class Vehicle{
 
 function processValue(value: string | number): number{
     if(typeof value === 'string'){
-        return value.length;
+        return value.length
     }else{
         return value *2
     }
 }
+
+// problem-06
+
+interface Product{
+    name: string;
+    price: number;
+  }
+  
+  function getMostExpensiveProduct(products: Product[]): Product | null{
+    if (products.length === 0){
+      return null
+    }
+  
+    let mostExpensive = products[0]
+  
+    for (const product of products){
+      if (product.price > mostExpensive.price){
+        mostExpensive = product
+      }
+    }
+    return mostExpensive
+  }
+
+//   Problem-07
+
+enum Day{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+  function getDayType(day: Day): string{
+    if (day === Day.Saturday || day === Day.Sunday) {
+      return "Weekend"
+    }
+    return "Weekday"
+  }
+
+//   Problem-08
+
+async function squareAsync(n: number): Promise<number>{
+    if (n < 0) {
+      throw new Error("Negative number not allowed")
+    }
+  
+    return new Promise((resolve) =>{
+      setTimeout(() =>{
+        resolve(n * n)
+      }, 1000)
+    })
+  }
+
+
+
 
